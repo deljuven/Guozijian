1111447a83aec54c7eafa2b4b2d4353866aa4ac6
deljuven <delforzajuven@gmail.com>
2016-11-13 16:39:02 +0800

    feat: add new file for learning flask. add .pyc to gitignore

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect(url_for('index'))
 
+
+@app.route('/hello')
+def hello():
+    return render_template("hello.html", username = 'test')
+
+
+@app.route('/index')
+def index():
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run()
