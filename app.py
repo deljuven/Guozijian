0cc21aa3919746104edfcdbdabb8c459c7eb5554
deljuven <delforzajuven@gmail.com>
2016-11-14 16:57:03 +0800

    feat: add bootstrap support

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, redirect, url_for
 
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
+def home():
     return redirect(url_for('index'))
 
 
 @app.route('/hello')
 def hello():
-    return render_template("hello.html", username = 'test')
+    return render_template("guozijian/login.html", username='test')
 
 
 @app.route('/index')
 def index():
     return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True)
