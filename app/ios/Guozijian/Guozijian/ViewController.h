3cfad692e01277984963a7c17c790b868c6cca01
deljuven <delforzajuven@gmail.com>
2017-01-19 15:14:11 +0800

    feat: add android app and ios code

---
+++ app/ios/Guozijian/Guozijian/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Guozijian
+//
+//  Created by 李然 on 2017/1/3.
+//  Copyright © 2017年 李然. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIWebView *wenView;
+
+@end
+
